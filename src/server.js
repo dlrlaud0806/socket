248e20c143030df1001194aba405f9ffaf533bad
giem <dlrlaud0806@gmail.com>
2021-10-06 19:19:35 +0900

    socket io added

---
+++ src/server.js
@@ -0,0 +1,40 @@
+import http from "http";
+import SocketIO from "socket.io";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log("Listening on port 3000");
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", socket => {
+    console.log(socket);
+})
+//const sockets = [];
+//const wss = new WebSocket.Server({server});
+
+// wss.on("connection", (socket)=> {
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser");
+//     socket.on("close", () => console.log("disconnected from the Browser"));
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch(message.type) {
+//             case "new_message":
+//                 sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//         }
+//     });
+// });
+
+httpServer.listen(3000, handleListen);
