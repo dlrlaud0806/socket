1e3243e95ca3b3bffd7b1404874548d726ac179e
giem <dlrlaud0806@gmail.com>
2021-10-10 13:36:21 +0900

    camera change added, audio mute added

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,59 +1,86 @@
 const socket = io();
+const myFace = document.getElementById("myFace");
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+const cameraBtn = document.getElementById("camera")
+const muteBtn = document.getElementById("mute")
+const camerasSelect = document.getElementById("cameras");
 
-room.hidden = true;
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-let roomName;
-
-function addMessage(message) {
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerText = message;
-    ul.appendChild(li);
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        const currentCamera = myStream.getVideoTracks()[0];
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            if (currentCamera.label === camera.label) {
+                option.selected = true;
+              }
+            camerasSelect.appendChild(option);
+        });
+    }
+    catch(e) {
+        console.log(e);
+    }
 }
 
-function handleMessageSubmit(event) {
-    event.preventDefault();
-    const input = room.querySelector("#msg input");
-    const value = input.value;
-    socket.emit("new_message", input.value, roomName, () => {
-      addMessage(`You: ${value}`);
-    });
-    input.value = "";
+async function getMedia(deviceId){
+    const initialConstrains = {
+        audio: true,
+        video: { facingMode: "user" },
+      };
+      const cameraConstraints = {
+        audio: true,
+        video: { deviceId: { exact: deviceId } },
+      };
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstrains
+          );
+          myFace.srcObject = myStream;
+          if (!deviceId) {
+            await getCameras();
+          }
+    }
+    catch(e) {
+        console.log(e)
+    }
 }
 
-function showRoom() {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName}`;
-  const msgform = room.querySelector("#msg");
-  msgform.addEventListener("submit", handleMessageSubmit);
+getMedia();
+
+function handleCameraClick() {
+    myStream.getVideoTracks().forEach((track)=> (track.enabled = !track.enabled));
+    if(!cameraOff) {
+        cameraBtn.innerText = "Turn Camera off";
+        cameraOff = true;
+    }
+    else {
+        cameraBtn,innerText = "Turn Camera on"
+        cameraOff = false;
+    }
 }
 
-function handleRoomSubmit(event) {
-  event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
-  input.value = "";
+function handleMuteClick() {
+    myStream.getAudioTracks().forEach((track)=> (track.enabled = !track.enabled));
+    if(!muted) {
+        muteBtn.innerText = "Unmute"
+        muted = true;
+    }
+    else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
 }
 
-form.addEventListener("submit", handleRoomSubmit);
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value);
+  }
 
-socket.on("welcome", (user, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    addMessage(`${user} arrived`);
-})
-
-socket.on("bye", (left, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    addMessage(`${user} left ㅠㅠ`);
-  });
-  
-socket.on("new_message", addMessage);
+cameraBtn.addEventListener("click", handleCameraClick);
+muteBtn.addEventListener("click", handleMuteClick);
